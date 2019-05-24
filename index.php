467b8f1a5fb3d962d97290185abe8aee27b535b0
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-24 23:01:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include("haut.php"); ?>
+
+
+
+<div id="photo_principal">
+
+	
+	
+
+
+
+
+
+</div>
+
+<div id ="principal">
+
+	<h2 id="actu">Actualitée</h2>
+
+
+	<div class="container-fluid" style="padding: 0px">
+
+		<div class="row" >
+
+			<div class="col-sm-4" style="background: red;height: 50px">
+				
+			</div>
+			<div class="col-sm-4" style="background: blue;height: 50px">
+				
+			</div>
+			<div class="col-sm-4" style="background: yellow;height: 50px">
+				
+			</div>
+
+			
+
+
+
+
+			
+			
+
+
+		</div>
+		
+
+
+	</div>
+
+
+	
+
+
+
+
+</div>
+
+
+
+
+
+<script type="text/javascript" src="javascript.js"></script>
+</body>
+</html>
