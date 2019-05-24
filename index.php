fccdd76ea6ae57c04e3860d9a63553ce601a18f3
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-24 23:15:15 +0200

    effet parallax

--- index.php
+++ index.php
@@ -22,13 +22,19 @@
 
 		<div class="row" >
 
-			<div class="col-sm-4" style="background: red;height: 50px">
+			<div class="col-sm-4">
+
+
 				
 			</div>
-			<div class="col-sm-4" style="background: blue;height: 50px">
+			<div class="col-sm-4">
+
+
 				
 			</div>
-			<div class="col-sm-4" style="background: yellow;height: 50px">
+			<div class="col-sm-4">
+
+				
 				
 			</div>
 
@@ -58,7 +64,11 @@
 
 
 
-
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.parallax-1.1.3.js"></script>
+<script type="text/javascript" src="scripts/jquery.localscroll-1.2.7-min.js"></script>
+<script type="text/javascript" src="scripts/jquery.scrollTo-1.4.2-min.js"></script>
 <script type="text/javascript" src="javascript.js"></script>
 </body>
 </html>
---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+
+	$('#photo_principal').parallax("50%", 0.6,true);
+	$('#principal').parallax("50%", 0.8,true);
+
+
+
+
+})
