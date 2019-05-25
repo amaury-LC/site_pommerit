04f02dffdd446fbfb4a38c42cb51f31f085afbcd
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-25 12:09:22 +0200

    actu

--- index.php
+++ index.php
@@ -18,11 +18,41 @@
 	<h2 id="actu">Actualitée</h2>
 
 
-	<div class="container-fluid" style="padding: 0px">
+	<div class="container" style="padding: 0px">
 
-		<div class="row" >
+		<div class="row" style="margin: 0px;padding: 0px" >
 
-			<div class="col-sm-4">
+			<div class="col-sm-4" >
+
+				<div class="card_principal" style="border-radius: 5px;">
+
+					<div class="card1 photo1">
+
+
+					</div>
+					<div class="card2">
+
+						<div>
+
+						<div class="titre_actu" >
+
+							<div style="height: 5px;width: 80%; border-bottom: 1px solid #F0F0F0;display: inline;margin-top: 12px;"></div>
+
+							<span class="titre_span" style="width: 100%">Démarche</span>
+
+							<div style="height: 5px;width: 80%; border-bottom: 1px solid #F0F0F0;display: inline;margin-top: 12px;"></div>
+							
+
+						</div>
+						<p class="text_actu">Élections européennes, ce qu'il faut savoir</p>
+
+					</div>
+						
+
+
+					</div>
+
+				</div>
 
 
 				
@@ -30,9 +60,28 @@
 			<div class="col-sm-4">
 
 
+				<div style="border-radius: 5px;">
+
+					<div class="card1">
+
+
+					</div>
+					
+
+				</div>
+
 				
 			</div>
 			<div class="col-sm-4">
+
+				<div style="border-radius: 5px;">
+
+					<div class="card1">
+
+
+					</div>
+
+				</div>
 
 				
 				
